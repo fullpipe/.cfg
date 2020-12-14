36acd07eeff05146d1b2ceff528be797702eeaf7
Eugene Bravov <eugene.bravov@gmail.com>
2020-12-14 10:23:26 +0300

    Add hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,13 @@
+hs.loadSpoon('Caps2Esc'):start()
+hs.loadSpoon('DownloadAtp'):start('/Volumes/U5/Datacasts')
+
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
+
+
+--hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+--  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+--end)
+
+--hs.hotkey.bind({"cmd"}, "return", function() hs.application.launchOrFocus("Alacritty") end)
