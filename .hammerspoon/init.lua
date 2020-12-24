c98512a10e555751ff96aa46292bad2e1b084b2d
Eugene Bravov <eugene.bravov@gmail.com>
2020-12-24 21:53:29 +0300

    Add some natural scrolling

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,10 +1,22 @@
 hs.loadSpoon('Caps2Esc'):start()
 hs.loadSpoon('DownloadAtp'):start('/Volumes/U5/Datacasts')
+hs.loadSpoon('RightLayout'):add("Alacritty", "ABC"):add("Slack", "Russian"):start()
 
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
-
+local function catcher(event)
+    if event:getFlags()['cmd'] and event:getCharacters() == "h" then
+        return true, {hs.eventtap.event.newScrollEvent({3, 0}, {}, "line")}
+    elseif event:getFlags()['cmd'] and event:getCharacters() == "l" then
+        return true, {hs.eventtap.event.newScrollEvent({-3, 0}, {}, "line")}
+    elseif event:getFlags()['cmd'] and event:getCharacters() == "j" then
+        return true, {hs.eventtap.event.newScrollEvent({0, -3}, {}, "line")}
+    elseif event:getFlags()['cmd'] and event:getCharacters() == "k" then
+        return true, {hs.eventtap.event.newScrollEvent({0, 3}, {}, "line")}
+    end
+end
+hs.eventtap.new({hs.eventtap.event.types.keyDown}, catcher):start()
 
 --hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
 --  hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
