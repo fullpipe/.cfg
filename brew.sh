42ce0687b64e0945ce56e2fc8df3ac57d0b48427
fullpipe <eugene.bravov@gmail.com>
2021-08-23 13:19:11 +0300

    Add brew install

---
+++ brew.sh
@@ -0,0 +1,8 @@
+brew install starship
+brew install svn
+brew tap homebrew/cask-fonts
+brew install --cask font-source-code-pro
+brew install --cask font-sauce-code-pro-nerd-font
+brew install node
+brew install go
+brew install fzf
